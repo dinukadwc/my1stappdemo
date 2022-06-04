9510a5bc9768517c08bbb7eaa0efeb7a00600ff5
dinukadwc <dinukadwc@gmail.com>
2022-06-04 22:16:28 +0530

    first commit

---
+++ src/components/RoundedButton/RoundedButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RoundedButton() {
+  return (
+    <button>Click to continue</button>
+  );
+}
+
+export default RoundedButton;
---
+++ src/pages/Home/HomeComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import RoundedButton from '../../components/RoundedButton/RoundedButton';
+
+function HomeComponent() {
+  return (
+    <div>
+        <h1>This is Home Page</h1>
+        <RoundedButton/>
+    </div>
+  );
+}
+
+export default HomeComponent;
